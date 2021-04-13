4c8786d48e57fd8c0275d1fd90415ec89a0e7dba
Trancongminh3006 <minhlinhtinhtp@gmail.com>
2021-04-14 02:09:16 +0700

    put version 2

--- demo_crawler/spiders/baovietsc.py
+++ demo_crawler/spiders/baovietsc.py
@@ -63,11 +63,11 @@
 
 
 #---------------------------------------------------------------------------------------
-
+#  version 1
 import json
 import scrapy
 from datetime import datetime
-# import urllib
+import time
 
 OUTPUT_FILENAME = 'output/baovietsc/baovietsc_{}.txt'.format(datetime.now().strftime('%Y%m%d_%H%M%S'))
 
@@ -75,19 +75,22 @@ OUTPUT_FILENAME = 'output/baovietsc/baovietsc_{}.txt'.format(datetime.now().strf
 class VnexpressSpider(scrapy.Spider):
     name = 'baovietsc'
     allowed_domains = ['bvsc.com.vn']
-    start_urls = ['https://bvsc.com.vn/Reports/8402/b.aspx']
+    start_urls = ['https://bvsc.com.vn/Reports/1/b.aspx']
     CRAWLED_COUNT = 0
-    report_id = 0
+    report_id = 8000
 
     def parse(self, response):
         print("alo alo alo")
+        # time.sleep(2)
         if response.status == 200 and response.css('meta[name="description"]::attr("content")').get() == 'Chi tiết báo cáo':
             print('Crawling from:', response.url)
             data = {
                 'link': response.url,
-                'nguồn':response.css('[id="ctl00_webPartManager_wp1449244041_wp303232192_lnkSource"]::text').get(),
+                'nguồn': response.css('[id="ctl00_webPartManager_wp1449244041_wp303232192_lnkSource"]::text').get(),
                 # 'ngày':response.css('td.NoPadding > table > tbody > tr').getall()[5],
-                'ngành':response.css('[id="ctl00_webPartManager_wp1449244041_wp303232192_lnkSector"]::text').get(),
+                'ngày': response.xpath('//td//table//tbody//tr//td/text()').get(),
+                'ngành': response.css('[id="ctl00_webPartManager_wp1449244041_wp303232192_lnkSector"]::text').get(),
+                'doanh_nghiep': response.css('[id="ctl00_webPartManager_wp1449244041_wp303232192_lnkSymbol"]::text').get(),
                 'pdf':'https://bvsc.com.vn'+ response.css('td.report_row_last > div > a::attr("href")').get(),
             }
             with open(OUTPUT_FILENAME, 'a', encoding='utf8') as f:
@@ -96,8 +99,71 @@ class VnexpressSpider(scrapy.Spider):
                 self.CRAWLED_COUNT += 1
                 self.crawler.stats.set_value('CRAWLED_COUNT', self.CRAWLED_COUNT)
                 print('SUCCESS:', response.url)
-        report_id = self.report_id + 1
-        href = self.start_urls[0][:28] + str(report_id) + self.start_urls[0][-7:]
-        print(href)
-        # # href = urllib.quote(href)
-        yield response.follow(href, callback=self.parse)
+
+        self.report_id = self.report_id + 1
+        print("--------------------------------------------------------------------------")
+        print(self.report_id)
+        print("--------------------------------------------------------------------------")
+        href = self.start_urls[0][:28] + str(self.report_id) + self.start_urls[0][-7:]
+        yield response.follow(href,meta = {
+                  'dont_redirect': True,
+                  'handle_httpstatus_list': [302]
+              }, callback=self.parse)
+
+
+
+# -------------------------------------------------------------------------
+# import json
+# import scrapy
+# import pandas as pd
+# from datetime import datetime
+# # import urllib
+
+# OUTPUT_FILENAME = 'output/baovietsc/baovietsc_{}.xlsx'.format(datetime.now().strftime('%Y%m%d_%H%M%S'))
+
+
+
+# link_list = []
+# nguon_list=[]
+# nganh_list=[]
+# pdf_list=[]
+# class VnexpressSpider(scrapy.Spider):
+#     name = 'baovietsc'
+#     allowed_domains = ['bvsc.com.vn']
+#     start_urls = ['https://bvsc.com.vn/Reports/1/b.aspx']
+#     CRAWLED_COUNT = 0
+#     report_id = 8000
+
+#     def parse(self, response):
+#         print("alo alo alo")
+#         if response.status == 200 and response.css('meta[name="description"]::attr("content")').get() == 'Chi tiết báo cáo':
+#             print('Crawling from:', response.url)
+#             link = response.url
+#             nguon = response.css('[id="ctl00_webPartManager_wp1449244041_wp303232192_lnkSource"]::text').get()
+#             # 'ngày':response.css('td.NoPadding > table > tbody > tr').getall()[5],
+#             nganh = response.css('[id="ctl00_webPartManager_wp1449244041_wp303232192_lnkSector"]::text').get()
+#             pdf = 'https://bvsc.com.vn'+ response.css('td.report_row_last > div > a::attr("href")').get()
+
+#             link_list.append(link)
+#             nguon_list.append(nguon)
+#             nganh_list.append(nganh)
+#             pdf_list.append(pdf)
+
+#         self.report_id = self.report_id + 1
+#         print("--------------------------------------------------------------------------")
+#         print(self.report_id)
+#         print("--------------------------------------------------------------------------")
+#         href = self.start_urls[0][:28] + str(self.report_id) + self.start_urls[0][-7:]
+#         yield response.follow(href,meta = {
+#                   'dont_redirect': True,
+#                   'handle_httpstatus_list': [302]
+#               }, callback=self.parse)
+
+#         df = pd.DataFrame({
+#             'link': link_list,
+#             'nguon':nguon_list,
+#             'nganh':nganh_list,
+#             'pdf':pdf_list
+#         })
+
+#         df.to_excel(OUTPUT_FILENAME,encoding='utf8',index = False)
